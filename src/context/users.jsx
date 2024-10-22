37352fc0e064929bed40efa4abeff106a378beb4
CryptozDev <cryptozdev@gmail.com>
2024-10-22 21:05:00 +0700

    Delete Source

--- src/context/users.jsx
+++
@@ -1,8 +0,0 @@
-const users = [
-    { user: 'jarukit', pass: '1234', role: 'admin', token: 'user' }
-  ];
-  
-  export function verifyUser(user, pass) {
-    const userFound = users.find((u) => u.user === user && u.pass === pass);
-    return userFound ? { role: userFound.role, token: userFound.token } : null;
-  }  
